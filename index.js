00f2e0053eae28039a8e61a91b99a1f00750ac2b
mdihbinah <mdihbinah@gmail.com>
2026-05-21 20:02:05 +0600

    add JWT Authentication.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs')
 dotenv.config()
 const app = express()
 app.use(cors())
@@ -23,6 +24,41 @@ const client = new MongoClient(uri, {
   }
 });
 
+const JWKS = createRemoteJWKSet(
+  new URL('http://localhost:3000/api/auth/jwks')
+)
+
+const verifyToken = async(req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({
+      message: 'Unauthorized Access'
+    });
+  }
+  // console.log(authHeader)
+
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({
+      message: 'Invalid Token'
+    })
+  }
+  // console.log(token)
+  try {
+    const { payload } = await jwtVerify(token, JWKS)
+    console.log(payload)
+    
+  } catch (error) {
+    console.error('Token validation failed:', error)
+    return res.status(403).json({
+   message: 'Forbidden Access'
+})
+  }
+
+
+  next()
+};
+
 async function run() {
   try {
     await client.connect()
@@ -32,118 +68,106 @@ async function run() {
     const bookingCollection = db.collection('bookings')
     const myAddedCarsCollection = db.collection('my-added-cars')
 
-    app.post('/cars', async(req, res)=> {
-        const car = req.body
-        console.log(car)
-        
-        const result = await carsCollection.insertOne(car)
-        res.json(result)
-    })
+    // app.post('/cars', async (req, res) => {
+    //   const car = req.body
+    //   console.log(car)
 
-    app.post('/bookings', async(req, res)=> {
-        const booking = req.body
-        console.log(booking)
-        
-        const result = await bookingCollection.insertOne(booking)
-        if(booking.carId){
-          const count = await carsCollection.updateOne({_id: new ObjectId(booking.carId)}, {$inc: {bookingCount: 1}})
-          console.log(count)
-          
-        }
-        res.json(result)
-    })
-
-    app.post('/my-added-cars', async(req, res)=> {
-        const booking = req.body
-        console.log(booking)
-        const result = await myAddedCarsCollection.insertOne(booking)
-        res.json(result)
-    })
-
-
-
-    app.get('/cars', async(req, res) => {
-        const result = await carsCollection.find().toArray()
-        res.json(result)
-    })
-
-    // app.get('/car', async(req, res) => {
-    //   const search = req.query.search
-    //   // console.log(search)
-    //     const result = await carsCollection.find({
-    //       carName: {
-    //         $regex: search,
-    //         $options: 'i'
-    //       }
-    //     }).toArray()
-    //     res.json(result)
+    //   const result = await carsCollection.insertOne(car)
+    //   res.json(result)
     // })
 
-    // app.get('/car', async(req, res) => {
-    //   const type = req.query.type
-    //   console.log(type)
-    //     const result = await carsCollection.find({
-    //       carType: {
-    //         $regex: type,
-    //         $options: 'i'
-    //       }
-    //     }).toArray()
-    //     res.json(result)
-    // })
+    app.post('/bookings',verifyToken, async (req, res) => {
+      const booking = req.body
+      console.log(booking)
 
+      const result = await bookingCollection.insertOne(booking)
+      if (booking.carId) {
+        const count = await carsCollection.updateOne({ _id: new ObjectId(booking.carId) }, { $inc: { bookingCount: 1 } })
+        console.log(count)
 
-    app.get('/car', async(req, res) => {
-
-    const { search, type } = req.query;
-
-    let query = {};
-
-    // search by car name
-    if (search) {
-      query.carName = {
-        $regex: search,
-        $options: 'i'
-      };
-    }
-
-    // search by car type
-    if (type) {
-      query.carType = {
-        $regex: type,
-        $options: 'i'
-      };
-    }
-
-    const result = await carsCollection.find(query).toArray();
-
-    res.status(200).json(result);
-
-   });
-    app.get(`/my-added-cars/:userId`, async(req, res) => {
-        const {userId} = req.params
-        
-        const result = await myAddedCarsCollection.find({userId}).toArray()
-        res.json(result)
+      }
+      res.json(result)
     })
 
-    app.get('/bookings', async(req, res) => {
-        const result = await bookingCollection.find().toArray()
-        res.json(result)
-    })
-
-    app.get('/bookings/:userId', async(req, res) => {
-      const {userId} = req.params
-        const result = await bookingCollection.find({userId}).toArray()
-        res.json(result)
+    app.post('/my-added-cars',verifyToken, async (req, res) => {
+      const booking = req.body
+      console.log(booking)
+      const result = await myAddedCarsCollection.insertOne(booking)
+      res.json(result)
     })
 
 
 
-    app.patch(`/my-added-cars/:id`, async(req, res) => {
-      const {id} = req.params
+    app.get('/cars', async (req, res) => {
+      // const header = req.headers.authorization
+      // console.log(header)
+
+      const result = await carsCollection.find().toArray()
+      res.json(result)
+    })
+
+    app.get('/cars/:id',verifyToken, async (req, res) => {
+      const {id} = await req.params
+      console.log(id)
+      
+      const result = await carsCollection.findOne({_id: new ObjectId(id)})
+      console.log(result)
+      
+      res.json(result)
+
+    })
+
+    app.get('/car', async (req, res) => {
+
+      const { search, type } = req.query;
+
+      let query = {};
+
+      // search by car name
+      if (search) {
+        query.carName = {
+          $regex: search,
+          $options: 'i'
+        };
+      }
+
+      // search by car type
+      if (type) {
+        query.carType = {
+          $regex: type,
+          $options: 'i'
+        };
+      }
+
+      const result = await carsCollection.find(query).toArray();
+      res.status(200).json(result);
+    });
+
+    app.get(`/my-added-cars/:userId`,verifyToken, async (req, res) => {
+      const { userId } = req.params
+
+      const result = await myAddedCarsCollection.find({ userId }).toArray()
+      res.json(result)
+    })
+
+    app.get('/bookings',verifyToken, async (req, res) => {
+      const result = await bookingCollection.find().toArray()
+      res.json(result)
+    })
+
+    app.get('/bookings/:userId',verifyToken, async (req, res) => {
+      const { userId } = req.params
+      const result = await bookingCollection.find({ userId }).toArray()
+      res.json(result)
+    })
+
+
+
+    app.patch(`/my-added-cars/:id`,verifyToken, async (req, res) => {
+      const { id } = req.params
       const updateData = req.body
       const result = myAddedCarsCollection.updateOne(
-        {_id: new ObjectId(id)},
+        { _id: new ObjectId(id) },
         {
           $set: updateData
         }
@@ -153,26 +177,26 @@ async function run() {
     })
 
 
-    app.patch(`/cars/:id`, async(req, res) => {
-      const {id} = req.params
-      // const updateData = req.body
-      const result = myAddedCarsCollection.updateOne(
-        {_id: new ObjectId(id)},
-        {
-          $inc: {
-            bookingCount: +1
-          }
-        }
-      )
-      console.log(result)
-      res.json(result)
-    })
+    // app.patch(`/cars/:id`, async (req, res) => {
+    //   const { id } = req.params
+    //   // const updateData = req.body
+    //   const result = myAddedCarsCollection.updateOne(
+    //     { _id: new ObjectId(id) },
+    //     {
+    //       $inc: {
+    //         bookingCount: +1
+    //       }
+    //     }
+    //   )
+    //   console.log(result)
+    //   res.json(result)
+    // })
 
 
 
-    app.delete(`/my-added-cars/:id`, async(req, res) => {
-      const {id} = req.params
-      const result = myAddedCarsCollection.deleteOne({_id: new ObjectId(id)})
+    app.delete(`/my-added-cars/:id`,verifyToken, async (req, res) => {
+      const { id } = req.params
+      const result = myAddedCarsCollection.deleteOne({ _id: new ObjectId(id) })
 
       console.log(result)
       res.json(result)
@@ -193,16 +217,11 @@ run().catch(console.dir);
 
 
 
-
-
-
-
-
-app.get('/', (req, res) =>{
-    res.send('Server is running')
+app.get('/', (req, res) => {
+  res.send('Server is running')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
